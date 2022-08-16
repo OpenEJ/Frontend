59f4239c9ab32648f79c3c9a8bf5345a094ddc5b
Keith L <keithlehman@proton.me>
2022-08-16 10:08:29 -0400

    built Vue component template

---
+++ src/components/VueComponentTemplate.vue
@@ -0,0 +1,29 @@
+<!--
+
+Remember to:
+1. Change NewComponentName to the name of the component
+2. Import this component via the file relative file path into ../App.vue
+3. Declare the components under 'components' in App.vue
+
+-->
+
+<template>
+    <p>This new component works!</p>
+</template>
+
+<script lang="ts">
+//Just regular TS here...
+
+import Vue from 'vue-class-component';
+
+export default class NewComponentName extends Vue {
+    //declare variables and write functions here
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
