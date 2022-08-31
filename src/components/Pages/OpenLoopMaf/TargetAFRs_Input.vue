5e4c062a666166b438033c0890418af478169693
WDominic <dom0_o@protonmail.com>
2022-08-30 21:04:05 -0400

    input recieved from text boxes

--- src/components/Pages/OpenLoopMaf/TargetAFRs_Input.vue
+++ src/components/Pages/OpenLoopMaf/TargetAFRs_Input.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
-        <textarea id="load"></textarea>
-        <textarea id="rpm"></textarea>
-        <textarea id="target_afrs"></textarea>
-        <button v-on:click="readData()">Read Target Afr Data</button>
+        <label> Enter Load Values
+            <textarea id="load" @input="updateLoad($event.target.value)"></textarea>
+        </label>
+        <br>
+        <label> Enter Rpm Values
+            <textarea id="rpm" @input="updateRpm($event.target.value)"></textarea>
+        </label>
+        <br>
+        <label> Enter Target Afrs Values
+            <textarea id="targetafrs" @input="updateTargetAfrs($event.target.value)"></textarea>
+        </label>
+        <br>
+        <button v-on:click="parseData()">Read Target Afr Data</button>
     </div>
 </template>
 
@@ -11,8 +20,24 @@
 import { Vue } from 'vue-class-component';
 
 export default class TargetAFRs_Input extends Vue{
-    readData() {
+    load_str: string = "";
+    rpm_str: string = "";
+    targetafrs_str: string = "";
 
+    updateLoad(value: string) {
+        this.load_str = value;
+    }
+    updateRpm(value: string) {
+        this.rpm_str = value;
+    }
+    updateTargetAfrs(value: string) {
+        this.targetafrs_str = value;
+    }
+    
+    parseData() {
+        console.log(`Load: ${this.load_str}`)
+        console.log(`Rpm: ${this.rpm_str}`)
+        console.log(`Target Afrs: ${this.targetafrs_str}`)
     }
 }
 </script>
