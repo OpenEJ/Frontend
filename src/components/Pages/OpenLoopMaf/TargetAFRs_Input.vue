73b1d02903d6ba44de348e2bd4b9f27ee765457d
W-Dominic <dom0_o@protonmail.com>
2022-12-08 20:06:09 -0500

    changed to quasar input boxes

--- src/components/Pages/OpenLoopMaf/TargetAFRs_Input.vue
+++ src/components/Pages/OpenLoopMaf/TargetAFRs_Input.vue
@@ -1,23 +1,17 @@
 <template>
-    <div>
-        <label> Enter Load Values
-            <textarea id="load" @input="updateLoad($event.target.value)"></textarea>
-        </label>
-        <br>
-        <label> Enter Rpm Values
-            <textarea id="rpm" @input="updateRpm($event.target.value)"></textarea>
-        </label>
-        <br>
-        <label> Enter Target Afrs Values
-            <textarea id="targetafrs" @input="updateTargetAfrs($event.target.value)"></textarea>
-        </label>
-        <br>
-        <button v-on:click="parseData()">Parse Target Afr Data</button>
-    </div>
+        <div class="fit">
+            <q-input square outlined v-model="load_str" label="Enter Load Values"/>
+            <br>
+            <q-input square outlined v-model="rpm_str" label="Enter Rpm Values"/>
+            <br>
+            <q-input square outlined v-model="targetafrs_str" label="Enter Target AFRs"/>
+            <br>
+            <button v-on:click="parseData()">Parse Target Afr Data</button>
+        </div>
 </template>
-
 <script lang="ts">
 import { Vue } from 'vue-class-component';
+//import { ref } from 'vue';
 
 export default class TargetAFRs_Input extends Vue{
     load_str: string = "";
@@ -28,33 +22,32 @@ export default class TargetAFRs_Input extends Vue{
     rpm: number[] = [];
     targetafrs: number[][] = [];
 
-    updateLoad(value: string) {
-        this.load_str = value;
+    /*
+    setup () {
+        return {
+            text: ref('')
+        }
     }
-    updateRpm(value: string) {
-        this.rpm_str = value;
-    }
-    updateTargetAfrs(value: string) {
-        this.targetafrs_str = value;
-    }
+    */
     
     parseData() {
         //we will parse the data from each of the textarea boxes, and store them in lists
+        console.log(this.load_str);
+        console.log(this.rpm_str);
         this.load = this.load_str.split("\t").map(entry => { return Number(entry); });
-        this.rpm = this.rpm_str.split("\n").map(entry => { return Number(entry); });
-        this.targetafrs = this.targetafrs_str.split("\n").map(row => {
+        this.rpm = this.rpm_str.split(" ").map(entry => { return Number(entry); });
+        this.targetafrs = this.targetafrs_str.split(" ").map(row => {
             return row.split("\t").map( val => {
                 return Number(val);
             });
         });
-        //console.log(this.load);
-        //console.log(this.rpm);
-        //console.log(this.targetafrs);
+        console.log(this.load);
+        console.log(this.rpm);
+        console.log(this.targetafrs);
 
         this.$emit("targetafr_processed", {load: this.load, rpm: this.rpm, targetafrs: this.targetafrs});
     }
 }
 </script>
-
 <style scoped>
 </style>
